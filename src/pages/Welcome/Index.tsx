d850af72590c2e43ef06556300edb6b61f911177
Luis Felipe <luisfelipe.szn@gmail.com>
2022-03-11 14:01:12 -0300

    Navigate button to home

--- src/pages/Welcome/Index.tsx
+++ src/pages/Welcome/Index.tsx
@@ -3,7 +3,7 @@ import {Container, WelcomeButton, Wallpaper, TitleButton} from './styles';
 import Icon from 'react-native-vector-icons/AntDesign';
 import Background from '../../../public/images/wallpaper.png';
 
-export function Welcome({ navigation } : {navigation: any}){
+export function Welcome({ navigation } : { navigation: any }){
 
   return(
     <Container> 
