a87ca1f8f03941e63e6c08512ee8a4dbe51762b6
Pede.ai <luisfelipe.szn@gmail.com>
2022-04-12 11:45:52 -0300

    change route types file name

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { propsNavigationStack } from './Models/Index';
+import { propsNavigationStack } from './types';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
